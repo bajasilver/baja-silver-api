9ad4e23f8fba0d57816da605d0256eb12844b787
Andrew Waddell <20awaddell@gmail.com>
2023-03-29 22:53:27 -0400

    Step 2 should be completed

--- Baja.Silver.Api/Controllers/CatalogController.cs
+++ Baja.Silver.Api/Controllers/CatalogController.cs
@@ -4,11 +4,18 @@ using Baja.Silver.Domain.Catalog;
 namespace Baja.Silver.Api.Controllers {
     [ApiController]
     [Route("[controller]")]
-    public class CatalogController : ControllerBase {
-        
+    public class CatalogController : ControllerBase 
+    {
         [HttpGet]
-        public IActionResult GetItems() {
-            return Ok("hello world.");
+        public IActionResult GetItems() 
+        {
+            var items = new List<Item>()
+            {
+                new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State shorts.", "Nike", 44.99m)
+            };
+
+            return Ok(items);
         }
         
     }
