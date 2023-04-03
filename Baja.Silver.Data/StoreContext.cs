f4f0f307fc1720ddf11db4d250fb4b7cfa554b87
Andrew Waddell <20awaddell@gmail.com>
2023-04-03 19:57:29 -0400

    adding startup data

---
+++ Baja.Silver.Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Baja.Silver.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace Baja.Silver.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+            { }
+
+            public DbSet<Item> Items { get; set;}
+
+            protected override void OnModelCreating(ModelBuilder builder)
+            { 
+                base.OnModelCreating(builder);
+                DbInitializer.Initialize(builder);
+            }
+    }
+}
