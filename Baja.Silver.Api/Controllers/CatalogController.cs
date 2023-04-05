dab3fd0bd85f32943ce8a70f5a9ec02c2fe162e4
Andrew Waddell <20awaddell@gmail.com>
2023-04-05 19:00:46 -0400

    last step?

--- Baja.Silver.Api/Controllers/CatalogController.cs
+++ Baja.Silver.Api/Controllers/CatalogController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Baja.Silver.Domain.Catalog;
 using Baja.Silver.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace Baja.Silver.Api.Controllers {
     [ApiController]
@@ -36,23 +37,72 @@ namespace Baja.Silver.Api.Controllers {
         }
         [HttpPost]
         public IActionResult Post(Item item) {
-            return Created("/catalog/42", item);
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return Created($"/catalog/{item.Id}", item);
+            //return Created("/catalog/42", item);
+            
+            //test for post
+            // { "name": "Shoes",
+            // "description": "Running Shoes",
+            // "brand": "Nike",
+            // "price": 109.99
+            // }
         }
         [HttpPost("{id:int}/ratings")]
         public IActionResult PostRating(int id, [FromBody] Rating rating) {
-             var item = new Item("Shirt", "Ohio State shirt.", "Nike", 29.99m);
-            item.Id = id;
+
+            var item = _db.Items.Find(id);
+            if (item == null) 
+            {
+                return NotFound();
+            }
+
             item.AddRating(rating);
+            _db.SaveChanges();
 
             return Ok(item);
         }
         [HttpPut("id:int")]
-        public IActionResult Put(int id, Item item) {
+        public IActionResult PutItem(int id, [FromBody] Item item)
+        {
+            if (id != item.Id) 
+            {
+                return BadRequest();
+            }
+
+            if (_db.Items.Find(id) == null) 
+            {
+                return NotFound();
+            }
+
+            _db.Entry(item).State = EntityState.Modified;
+            _db.SaveChanges();
+
             return NoContent();
         }
-        [HttpDelete("id:int")]
-        public IActionResult Delete(int id) {
-            return NoContent();
+
+        // public IActionResult Put(int id, Item item) {
+        //     return NoContent();
+        // }
+        // [HttpDelete("id:int")]
+        // public IActionResult Delete(int id) {
+        //     return NoContent();
+        // }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteItem(int id)
+        {
+            var item = _db.Items.Find(id);
+            if(item == null)
+            {
+                return NotFound();
+            }
+
+            _db.Items.Remove(item);
+            _db.SaveChanges();
+
+            return Ok();
         }
         
     }
--- Baja.Silver.Domain/Catalog/Rating.cs
+++ Baja.Silver.Domain/Catalog/Rating.cs
@@ -1,12 +1,12 @@
 namespace Baja.Silver.Domain.Catalog;
 
 public class Rating {
-    public int ID { get; set; }
+    public int Id { get; set; }
     public int Stars { get; set; }
     public string UserName { get; set; }
     public string Review { get; set; }
 
-    public Rating(int stars, string userName, string review) {
+    public Rating(int id, int stars, string userName, string review) {
         if (stars < 1 || stars > 5) {
             throw new ArgumentException("Star rating must be an integer of: 1, 2, 3, 4, 5.");
         }
@@ -14,6 +14,7 @@ public class Rating {
             throw new ArgumentException("UserName cannot be null.");
         }
 
+        this.Id = id;
         this.Stars = stars;
         this.UserName = userName;
         this.Review = review;
