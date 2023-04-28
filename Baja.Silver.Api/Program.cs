2277bb8433f057683be9c681bb5209cc4ed4d326
jcaudill2001 <caudill.689@osu.edu>
2023-04-28 15:23:00 -0400

    Secure API Auth Problem

--- Baja.Silver.Api/Program.cs
+++ Baja.Silver.Api/Program.cs
@@ -1,12 +1,37 @@
 using Baja.Silver.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Sqlite;
+using Baja.Silver.Api.Security;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 
 var builder = WebApplication.CreateBuilder(args);
+
+String authority = builder.Configuration["Auth0:Authority"] ??
+    throw new ArgumentNullException("Auth0:Authority");
+
+string audience = builder.Configuration["Auth0:Audience"] ??
+    throw new ArgumentNullException("Auth0:Audience");
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        options.Authority = authority;
+        options.Audience = audience;
+    });
+builder.Services.AddAuthorization(options =>
+    {
+        options.AddPolicy("delete:catalog", policy =>
+            policy.RequireAuthenticatedUser().RequireClaim("scope", "delete:catalog"));
+    });
 builder.Services.AddDbContext<StoreContext>(
     options => options.UseSqlite("Data Source=../Registrar.sqlite", 
     b => b.MigrationsAssembly("Baja.Silver.Api"))
@@ -35,7 +60,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+
 app.UseCors();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
