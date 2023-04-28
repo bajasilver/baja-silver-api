47f18e5a53b92e58bd543ae200b87ac111b55e37
jcaudill2001 <caudill.689@osu.edu>
2023-04-28 14:32:06 -0400

    Add CORS Support

--- Baja.Silver.Api/Program.cs
+++ Baja.Silver.Api/Program.cs
@@ -11,6 +11,15 @@ builder.Services.AddDbContext<StoreContext>(
     options => options.UseSqlite("Data Source=../Registrar.sqlite", 
     b => b.MigrationsAssembly("Baja.Silver.Api"))
     );
+builder.Services.AddCors(options => 
+{
+    options.AddDefaultPolicy(builder => 
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,6 +34,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors();
 
 app.UseAuthorization();
 
